20942ffc82393f40f04d41e07a70bc70d93deef6
ZeyadZanaty <zeyadzanaty@Zeyads-MacBook-Pro.local>
2018-04-06 23:18:28 +0200

    Classification class added

---
+++ Classify.py
@@ -0,0 +1,57 @@
+class Classify:
+  """
+  Class for classifying data after vectorization
+  """
+  def __init__(self, tr_data,tst_data):
+    self.tr_data = tr_data
+    self.tst_data = tst_data
+    self.tr_vecs = [d[1] for d in self.tr_data]
+    self.tr_labels = [d[2] for d in self.tr_data]
+    self.tst_vecs = [d[1] for d in self.tst_data]
+    self.tst_labels = [d[2] for d in self.tst_data]
+
+  def KNN(self):
+    from sklearn.neighbors import KNeighborsClassifier
+    scores = []
+    scores.append([0,0])
+    knn = KNeighborsClassifier(n_neighbors=1, weights='distance').fit(self.tr_vecs, self.tr_labels)
+    h_score = knn.score(self.tst_vecs,self.tst_labels)
+    scores.append([1,h_score])
+    i=0
+    while scores[i+1][1] >= scores[i][1]:
+      i+=1
+      k=i+1
+      knn = KNeighborsClassifier(n_neighbors=k, weights='distance').fit(self.tr_vecs, self.tr_labels)
+      h_score = knn.score(self.tst_vecs,self.tst_labels)
+      scores.append([k,h_score])
+      print(scores)
+    return scores
+
+  def DecisionTrees(self):
+    from sklearn.tree import DecisionTreeClassifier
+    results = []
+    clf = DecisionTreeClassifier().fit(self.tr_vecs,self.tr_labels)
+    score = clf.score(self.tst_vecs,self.tst_labels)
+    results.append(['Gini',score])
+
+    clf = DecisionTreeClassifier(criterion='entropy').fit(self.tr_vecs,self.tr_labels)
+    score = clf.score(self.tst_vecs,self.tst_labels)
+    results.append(['Entropy', score])
+    return results
+
+  # def RandomForrests(self,tr_data,tst_data, criterion='gini'):
+  #   from sklearn.ensemble import RandomForestClassifier
+  #   i=0
+  #   n_trees = 10
+  #   clf = RandomForestClassifier(criterion=criterion,n_estimators=n_trees).fit(self.tr_vecs,self.tr_labels)
+  #   h_score = clf.score(self.tst_vecs,self.tst_labels)
+  #   scores = []
+  #   scores.append([0,0])
+  #   scores.append([n_trees, h_score])
+  #   while scores[i + 1][1] >= scores[i][1]:
+  #     n_trees+=10
+  #     clf = RandomForestClassifier(criterion=criterion, n_estimators=n_trees).fit(self.tr_vecs,self.tr_labels)
+  #     h_score = clf.score(self.tst_vecs,self.tst_labels)
+  #     scores.append([n_trees,h_score])
+  #     i+=1
+  #   return scores, n_trees,h_score
--- Main.py
+++ Main.py
@@ -1,9 +1,10 @@
 from DataSetReader import DataSetReader
 from PreProcess import PreProcess
 from Vectorizer import Vectorizer
+from Classify import Classify
 import numpy as np
 
-dsr = DataSetReader(directory="./aclImdb/")
+dsr = DataSetReader(directory="../aclImdb/")
 
 tr_data = dsr.labelled_string_data('train')
 # Data split by label
@@ -24,14 +25,23 @@ train_StemLemmatize = train_prp.lemmatize()
 vectorizer = Vectorizer(type='tfidf', fit_data=tr_small, params={'ngram_range': (1, 3)})
 tr_small_vecs = vectorizer.vectorize(tr_small)
 
+tst_data = dsr.labelled_string_data('test')
 
+tst_negative, tst_positive = tst_data[:len(tst_data)//2], tst_data[len(tst_data)//2:]
 
-# tst_data = dsr.labelled_string_data('test')
-# tst_prp = PreProcess(tst_data)
-# tr_data = tst_prp.tokenize()
-# tst_prp.remove_stopwords()
-# tst_StemPS = tst_prp.stemmingPS()
-# tst_StemLS = tst_prp.stemmingLS()
-# tst_StemSB = tst_prp.stemmingSB()
-# tst_StemLemmatize = tst_prp.lemmatize()
-# tst_negative, tst_positive = tst_data[:len(tst_data)//2], tst_data[len(tst_data)//2:]
+tst_small = tst_negative[:500]+tst_positive[:500]
+
+tst_prp = PreProcess(tst_small)
+tst_small = tst_prp.tokenize()
+tst_prp.remove_stopwords()
+tst_StemPS = tst_prp.stemmingPS()
+tst_StemLS = tst_prp.stemmingLS()
+tst_StemSB = tst_prp.stemmingSB()
+tst_StemLemmatize = tst_prp.lemmatize()
+
+tst_small_vecs = vectorizer.vectorize(tst_small)
+
+clf = Classify(tr_small_vecs,tst_small_vecs)
+
+score = clf.DecisionTrees()
+print(score)
