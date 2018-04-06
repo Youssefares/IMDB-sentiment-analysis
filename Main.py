9f5fc063d9f5c6004007221c599ee28a372ba183
tifa64 <krietallo@gmail.com>
2018-04-06 21:11:40 +0200

    Pre-processing of test data

--- Main.py
+++ Main.py
@@ -6,36 +6,13 @@ import numpy as np
 dsr = DataSetReader(directory="./aclImdb/")
 
 tr_data = dsr.labelled_string_data('train')
-
-prp = PreProcess(tr_data)
-print('Triainning data')
-print(tr_data[0])
-print()
-tr_data = prp.tokenize()
-print('Tokenized data')
-print(tr_data[0])
-print()
-prp.remove_stopwords()
-print('Remove stopwords')
-print(tr_data[0])
-print()
-dummy = prp.stemmingPS()
-print('Stemming data using Porter')
-print(dummy[0])
-print()
-dummy = prp.lemmatize()
-print('Lemmatization data')
-print(dummy[0])
-print()
-dummy = prp.stemmingLS()
-print('Stemming data using Lancaster')
-print(dummy[0])
-print()
-dummy = prp.stemmingSB()
-print('Stemming data using Snowball')
-print(dummy[0])
-print()
-
+train_prp = PreProcess(tr_data)
+tr_data = train_prp.tokenize()
+train_prp.remove_stopwords()
+train_StemPS = train_prp.stemmingPS()
+train_StemLS = train_prp.stemmingLS()
+train_StemSB = train_prp.stemmingSB()
+train_StemLemmatize = train_prp.lemmatize()
 
 # Data split by label
 tr_negative, tr_positive = tr_data[:len(tr_data)//2], tr_data[len(tr_data)//2:]
@@ -43,10 +20,16 @@ tr_negative, tr_positive = tr_data[:len(tr_data)//2], tr_data[len(tr_data)//2:]
 # small set of data of size 1000
 tr_small = tr_negative[:500]+tr_positive[:500]
 
-
 vectorizer = Vectorizer(type='count', params={'ngram_range': (1, 3)})
 
 tr_small_vecs = vectorizer.vectorize(tr_small)
 
 tst_data = dsr.labelled_string_data('test')
+tst_prp = PreProcess(tst_data)
+tr_data = tst_prp.tokenize()
+tst_prp.remove_stopwords()
+tst_StemPS = tst_prp.stemmingPS()
+tst_StemLS = tst_prp.stemmingLS()
+tst_StemSB = tst_prp.stemmingSB()
+tst_StemLemmatize = tst_prp.lemmatize()
 tst_negative, tst_positive = tst_data[:len(tst_data)//2], tst_data[len(tst_data)//2:]
