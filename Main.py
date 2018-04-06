abe013f92e89324eb6e225659203eb14b4e11fdc
Youssefares <Youssefares@ymail.com>
2018-04-06 20:53:14 +0200

    Trying out ngrams on a smaller dataset

--- Main.py
+++ Main.py
@@ -6,6 +6,7 @@ import numpy as np
 dsr = DataSetReader(directory="./aclImdb/")
 
 tr_data = dsr.labelled_string_data('train')
+
 prp = PreProcess(tr_data)
 print(tr_data[0])
 tr_data = prp.tokenize()
@@ -13,17 +14,16 @@ print(tr_data[0])
 prp.remove_stopwords()
 print(tr_data[0])
 
-vectorizer = Vectorizer(type='count')
-tr_data = vectorizer.vectorize(tr_data)
+# Data split by label
 tr_negative, tr_positive = tr_data[:len(tr_data)//2], tr_data[len(tr_data)//2:]
 
+# small set of data of size 1000
+tr_small = tr_negative[:500]+tr_positive[:500]
 
+
+vectorizer = Vectorizer(type='count', params={'ngram_range': (1, 3)})
+
+tr_small_vecs = vectorizer.vectorize(tr_small)
 
 tst_data = dsr.labelled_string_data('test')
-# prp_tst = PreProcess(tst_data)
-# print(tst_data[0])
-# tst_data = prp_tst.tokenize()
-# print(tst_data[0])
-# prp_tst.remove_stopwords()
-# print(tst_data[0])
 tst_negative, tst_positive = tst_data[:len(tst_data)//2], tst_data[len(tst_data)//2:]
