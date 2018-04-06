aafa653b9a4124c78fde32e547989b14c894996e
ZeyadZanaty <zeyadzanaty@Zeyads-MacBook-Pro.local>
2018-04-06 17:13:15 +0200

    altered main class

--- Main.py
+++ Main.py
@@ -1,18 +1,24 @@
 from DataSetReader import DataSetReader
 from PreProcess import PreProcess
 
-dsr = DataSetReader(directory="./aclImdb/")
+dsr = DataSetReader(directory="../aclImdb/")
 
 tr_data = dsr.labelled_string_data('train')
 prp = PreProcess(tr_data)
-
-tkn_tr_data = prp.tokenize(tr_data)
-prp.remove_stopwords(tkn_tr_data)
+print(tr_data[0])
+tr_data = prp.tokenize()
+print(tr_data[0])
+prp.remove_stopwords()
+print(tr_data[0])
 tr_negative, tr_positive = tr_data[:len(tr_data)//2], tr_data[len(tr_data)//2:]
 
 tst_data = dsr.labelled_string_data('test')
-tkn_tst_data = prp.tokenize(tst_data)
-prp.remove_stopwords(tkn_tst_data)
+prp_tst = PreProcess(tst_data)
+print(tst_data[0])
+tst_data = prp_tst.tokenize()
+print(tst_data[0])
+prp_tst.remove_stopwords()
+print(tst_data[0])
 tst_negative, tst_positive = tst_data[:len(tst_data)//2], tst_data[len(tst_data)//2:]
 
 
