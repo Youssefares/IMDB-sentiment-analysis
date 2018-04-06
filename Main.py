efcfdf1f9a2fde91c5b1446784140c4fe5f4db20
tifa64 <krietallo@gmail.com>
2018-04-06 20:58:29 +0200

    Lemmitization and Stemming

--- Main.py
+++ Main.py
@@ -1,15 +1,37 @@
 from DataSetReader import DataSetReader
 from PreProcess import PreProcess
 
-dsr = DataSetReader(directory="../aclImdb/")
+dsr = DataSetReader(directory="./aclImdb/")
 
 tr_data = dsr.labelled_string_data('train')
 prp = PreProcess(tr_data)
+print('Triainning data')
 print(tr_data[0])
+print()
 tr_data = prp.tokenize()
+print('Tokenized data')
 print(tr_data[0])
+print()
 prp.remove_stopwords()
+print('Remove stopwords')
 print(tr_data[0])
+print()
+dummy = prp.stemmingPS()
+print('Stemming data using Porter')
+print(dummy[0])
+print()
+dummy = prp.lemmatize()
+print('Lemmatization data')
+print(dummy[0])
+print()
+prp.stemmingLS()
+print('Stemming data using Lancaster')
+print(tr_data[0])
+print()
+prp.stemmingSB()
+print('Stemming data using Snowball')
+print(tr_data[0])
+print()
 tr_negative, tr_positive = tr_data[:len(tr_data)//2], tr_data[len(tr_data)//2:]
 
 tst_data = dsr.labelled_string_data('test')
--- PreProcess.py
+++ PreProcess.py
@@ -1,3 +1,5 @@
+import copy
+
 class PreProcess:
   """
   Class for pre-processing the review before passing it for analyzing
@@ -22,3 +24,55 @@ class PreProcess:
           temp.append(w)
       d[1] = temp
     return self.data
+
+  def stemmingPS(self):
+    from nltk.stem import PorterStemmer
+    ps = PorterStemmer()
+    temp = copy.deepcopy(self.data)
+    for i in range(len(temp)):
+        for j in range(len(temp[i][1])):
+            temp[i][1][j] = ps.stem(temp[i][1][j])
+    return temp
+
+  def stemmingLS(self):
+    from nltk.stem import LancasterStemmer
+    ls = LancasterStemmer()
+    temp = copy.deepcopy(self.data)
+    for i in range(len(temp)):
+        for j in range(len(temp[i][1])):
+            temp[i][1][j] = ls.stem(temp[i][1][j])
+    return temp
+
+  def stemmingSB(self):
+    from nltk.stem import SnowballStemmer
+    sb = SnowballStemmer("english")
+    temp = copy.deepcopy(self.data)
+    for i in range(len(temp)):
+        for j in range(len(temp[i][1])):
+            temp[i][1][j] = sb.stem(temp[i][1][j])
+    return temp
+
+  def get_pos(self, word):
+      from collections import Counter
+      from nltk.corpus import wordnet  # To get words in dictionary with their parts of speech
+
+      w_synsets = wordnet.synsets(word)
+
+      pos_counts = Counter()
+      pos_counts["n"] = len([item for item in w_synsets if item.pos() == "n"])
+      pos_counts["v"] = len([item for item in w_synsets if item.pos() == "v"])
+      pos_counts["a"] = len([item for item in w_synsets if item.pos() == "a"])
+      pos_counts["r"] = len([item for item in w_synsets if item.pos() == "r"])
+
+      most_common_pos_list = pos_counts.most_common(3)
+      return most_common_pos_list[0][0]  # first indexer for getting the top POS from list, second indexer for getting POS from tuple( POS: count )
+
+  def lemmatize(self):
+      from nltk.stem import WordNetLemmatizer  # lemmatizes word based on it's parts of speech
+
+      wnl = WordNetLemmatizer()
+      temp = copy.deepcopy(self.data)
+      for i in range(len(temp)):
+          for j in range(len(temp[i][1])):
+              temp[i][1][j] = wnl.lemmatize(temp[i][1][j], pos = self.get_pos(temp[i][1][j]))
+      return temp
