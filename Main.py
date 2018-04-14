c77bc2baa3ac4b1ffa45744197a2003a9fe1c641
ZeyadZanaty <zeyad_zanaty@hotmail.com>
2018-04-14 03:52:22 +0200

    Plotting

--- Main.py
+++ Main.py
@@ -6,6 +6,55 @@ from NewClassifier import Classifier
 import itertools
 import numpy as np
 import matplotlib.pyplot as plt
+from collections import OrderedDict
+
+def clean_string(str):
+  newstr = ''
+  noise = ['[',']','{','}',"'",'(',')',',']
+  for c in str:
+    if c == ' ':
+      newstr+='\n'
+    elif c not in noise:
+      newstr+= c
+  return newstr
+
+def get_plot_arrs(params,acc_num, items):
+  plt_arr = [acc[0] for acc in items]
+  plt_arr = [plt_arr[i] for i in range (0,acc_num)]
+  ticks_arr = [clean_string(acc[1].__str__())+clean_string(acc[2].__str__())+'\n Parameters: '+
+               clean_string(params.__str__())for acc in v]
+  ticks_arr = [ticks_arr[i] for i in range (0,acc_num)]
+  return plt_arr,ticks_arr
+
+def plot_clfs(width,max_num,acc_sep,param_sep):
+  for key, value in clf_dict.items():
+    print(key)
+    plt_acc = []
+    plt_tks = []
+    plt.figure()
+    for k, v in value.items():
+      plt.title(key.__str__())
+      plt_arr, plt_arr_ticks = get_plot_arrs(k,max_num, v)
+      print(plt_arr, plt_arr_ticks)
+      if not plt_acc:
+        plt_acc = plt_arr
+        plt_tks = plt_arr_ticks
+      else:
+        plt_acc += plt_arr
+        plt_tks += plt_arr_ticks
+    ind = np.array(1)
+    x = 1
+    for i in range(1, len(plt_acc)):
+      if ((i) % max_num == 0):
+        x += param_sep * width
+      else:
+        x += acc_sep * width
+      ind = np.append(ind, x)
+    ind = ind[0:len(plt_acc)]
+    plt.bar(ind, plt_acc, width=width)
+    plt.xticks(ind, plt_tks)
+    plt.show()
+
 
 dsr = DataSetReader(directory="../aclImdb/")
 
@@ -26,25 +75,30 @@ tst_small = tst_negative[:500]+tst_positive[:500]
 
 # Preprocessing combinations
 cleaning_operations = ['remove_stopwords','lemmatize','stemmingLS','stemmingPS','stemmingSB'];
-combinations = [i  for j in range(len(cleaning_operations)) for i in itertools.combinations(cleaning_operations,j+1)]
+cleaning_operations = ['remove_stopwords','lemmatize'];
 
-# Vectorization combinations
-vec_list = [['tfidf',{}],['count',{}],['wordembedd',{}],['fasttext',{}]]
+combinations = [i for j in range(len(cleaning_operations)) for i in itertools.combinations(cleaning_operations,j+1)]
 
-# Classifier combinations
+# Vectorizers
+vec_list = [['tfidf',{}],['count',{}],['wordembedd',{'min_count':1}],['fasttext',{}]]
+
+# Classifiers
 clf_list = [['KNN',{}],['SVC',{}],['DecisionTree',{}],['RandomForestClassifier',{}],['LogisticRegression',{}]
   ,['MLP',{}],['AdaBoost',{}],['Bagging',{}]]
 
-clf_list = [['KNN',{'n_neighbors': [i for i in range(1, 4, 2)]}]]
-vec_list = [['tfidf',{}]]
+clf_list = [['RandomForestClassifier',{'n_estimators': [i for i in range(10,30,10)]}],
+            ['KNN',{'n_neighbors':[i for i in range (1,6,2)]}]
+           ]
+vec_list = [['wordembedd',{'min_count':1}],['tfidf',{}]]
 
 
 # ex: {'KNN': [(acc, vectorization, preprocessing_ops)], 'LR': ... }
 clf_dict = {clf[0]: {} for clf in clf_list}
 for vec in vec_list:
-  print('Vectorization technique: '+vec[0].__str__())
+  print()
+  print('Vectorization: '+vec[0].__str__())
   for combination in combinations:
-    print('Preprocessing : ' + combination.__str__())
+    print('Preprocessing: ' + combination.__str__())
     tr_prp = PreProcess(tr_small)
     tst_prp = PreProcess(tst_small)
 
@@ -67,6 +121,19 @@ for vec in vec_list:
       # should have dict of clfs contatining dict of params with all accuracies & methods tried
       for key, value in params_accs.items():
         if key in clf_dict[cl[0]]:
-          clf_dict[cl[0]][key] += [(value, vec, combination)]
+          clf_dict[cl[0]][key] += [(value, vec,combination)]
         else:
           clf_dict[cl[0]][key] = [(value, vec, combination)]
+
+for key,value in clf_dict.items():
+  for k,v in value.items():
+    v = (sorted(v, key=lambda x: x[0], reverse=True))
+    clf_dict[key][k] = v
+
+print(clf_dict)
+
+plot_clfs(0.15,3,1.2,2.8)
+# width = 0.15
+# max_num = 3
+# acc_sep = 1.2
+# param_sep = 2.8
